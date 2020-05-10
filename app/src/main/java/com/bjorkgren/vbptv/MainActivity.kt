c3b57633cd626b9a3878902f018b620aa63aae90
Tobias Björkgren <bjorkgren.tobias@gmail.com>
2020-05-10 16:58:05 +0200

    fetch texttv pages one at a time, update ui

--- app/src/main/java/com/bjorkgren/vbptv/MainActivity.kt
+++ app/src/main/java/com/bjorkgren/vbptv/MainActivity.kt
@@ -3,10 +3,12 @@ package com.bjorkgren.vbptv
 import android.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.util.Log
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -16,7 +18,7 @@ import com.bjorkgren.vbptv.model.TVProgram
 class MainActivity : AppCompatActivity() {
 
     val wantedChannels = listOf("SVT1", "SVT2", "TV3", "TV4", "Kanal 5")
-    val textTvPages = listOf(650, 651, 652)
+    var textTvPages = listOf(650, 651, 652, 653)
     val schedule = hashMapOf<String, MutableList<TVProgram>>()
     var lastAddedChannel = ""
 
@@ -33,26 +35,33 @@ class MainActivity : AppCompatActivity() {
         }
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        for(page in textTvPages){
-            //add a request to queue
-            queue.add(
-                StringRequest(
-                    Request.Method.GET, "https://www.svt.se/svttext/webL/pages/$page.html",
-                    Response.Listener<String> { response ->
-                        parseSVTTextPage(response)
-                        updateUI()
-                    },
-                    Response.ErrorListener { error ->
-                        Log.e("Error", error.message)
-                    })
-            )
-        }
+        //Fetch pages in order to avoid the async sort of lists n stuff
+        //Warning, this is a bit recursive...
+        fetchPages(queue, textTvPages)
     }
 
-    //TODO: Programmatically create the "Pågående program"-tab
-        //this will also have the channel...
-    //TODO: loop and Programatically create the following specific channel tabs
-        //becase this layout will be more like a table with just time + name
+    fun fetchPages(queue: RequestQueue, pages: List<Int>){
+        Log.e("fetches", "FETCH ${pages.first()}")
+        queue.add(
+            StringRequest(
+                Request.Method.GET, "https://www.svt.se/svttext/webL/pages/${pages.first()}.html",
+                Response.Listener<String> { response ->
+                    parseSVTTextPage(response)
+                    updateUI()
+                    if(pages.size > 1){
+                        fetchPages(queue, pages.drop(1))
+                    }
+                },
+                Response.ErrorListener { error ->
+                    Log.e("Error", error.message)
+                })
+        )
+    }
+
+    //TODO: Skapa en linear layout per kanal, som bara visar program 0, med tid,
+    // håller man ner nånstans i uit så visas nästkommande tills man släpper
+
+    //TODO: framtida funktionalitet:
     //Första tab:en är "pågående program", ett program per kanal, precis som de ser ut på SVT text 650
     //Nästa tab:en är för SVT1, sen en tab per kanal
     //      pga att vissa kanaler har många korta program (SVT2) o vissa har få o långa...
@@ -96,12 +105,13 @@ class MainActivity : AppCompatActivity() {
                 schedule[lastAddedChannel]!!.last()!!.headline += " $ending"
 
             }else{
-                val time = trimmed.substring(0, line.indexOf('<') - 2)
+                val time = trimmed.substring(0, line.indexOf('<') - 1).trim()
                 val headlineStart = trimmed.indexOf('>') +1
                 val headline = trimmed.substring(headlineStart, trimmed.indexOf('<', startIndex = headlineStart)).trim()
                 val channelEnd = trimmed.lastIndexOf('<')
-                val channel = trimmed.substring(trimmed.lastIndexOf('>', startIndex = channelEnd) + 1, channelEnd)
+                val channel = trimmed.substring(trimmed.lastIndexOf('>', startIndex = channelEnd) + 1, channelEnd).trim()
                 schedule[channel]?.add(TVProgram(time, headline))
+                Log.e("added", channel)
                 lastAddedChannel = channel
                 //Log.w("a find!", "$time^$headline^$channel")
             }
@@ -109,20 +119,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateUI(){
-        var i = 0
       // val listLayout = findViewById<LinearLayout>(R.id.channels)
-        for(wanted in wantedChannels){
-            //Skapa en linear layout per kanal, som bara visar program 0, håller man ner nånstans i uit så visas nästkommande tills man släpper
-            //schedule[wanted] = mutableListOf<TVProgram>()
-
-            //TODO: DO not add UI, just update the items..
-            val itemLayout = LinearLayout(this)
-            itemLayout.setLayoutParams(LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.0f))
-            itemLayout.orientation = LinearLayout.HORIZONTAL
-            val txtChannel = TextView(this)
-            txtChannel.text = (++i).toString()
-            itemLayout.addView(txtChannel)
-           // listLayout.addView(itemLayout)
+        val uiChannels = listOf(R.id.svt1, R.id.svt2, R.id.tv3, R.id.tv4, R.id.kanal5)
+        for(x in 0 until uiChannels.size){
+            val channelLayout = findViewById<ConstraintLayout>(uiChannels[x])
+            val program = schedule[wantedChannels[x]]?.first()
+            if(program != null){
+                channelLayout.findViewById<TextView>(R.id.txtChannelNo).text = (x+1).toString()
+                val time = channelLayout.findViewById<TextView>(R.id.txtTime)
+                val headline = channelLayout.findViewById<TextView>(R.id.txtHeadline)
+                time.text = program.begins
+                headline.text = program.headline
+            }
         }
     }
 }
